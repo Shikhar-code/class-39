a265dcafc175eda6bdd39a8020f9fd98a63d2e4b
Shikhar <shikhar.sourabh@gmail.com>
2021-01-09 16:51:26 +0530

    added images

--- sketch.js
+++ sketch.js
@@ -8,8 +8,15 @@ var database;
 
 var form, player, game;
 
-var cars, car1, car2, car3, car4;
-
+var cars, car1, car2, car3, car4,car1I,car2I,car3I,car4I,track,ground
+function preload(){
+  car1I=loadImage("images/car1.png")
+  car2I=loadImage("images/car2.png")
+  car3I=loadImage("images/car3.png")
+  car4I=loadImage("images/car4.png")
+  track=loadImage("images/track.jpg")
+  ground=loadImage("images/ground.png")
+}
 
 function setup(){
   canvas = createCanvas(displayWidth - 20, displayHeight-30);
@@ -27,5 +34,8 @@ function draw(){
   if(gameState === 1){
     clear();
     game.play();
+  }
+  if(gameState===2){
+    game.end()
   }
 }
